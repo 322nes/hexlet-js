bc49d37193ad85ce292d18962ed57c58c54df9b7
Pavel Nesen <pavelnesen@yandex.ru>
2023-10-11 14:08:47 +0300

    add test

---
+++ test/test17.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+
+const freeEmailDomains = [
+  'gmail.com',
+  'yandex.ru',
+  'hotmail.com',
+  'yahoo.com',
+];
+
+const getFreeDomainsCount = (emails) => emails
+  .map((email) => {
+    const [, domain] = email.split('@');
+    return domain;
+  })
+  .filter((domain) => freeEmailDomains.includes(domain))
+  .reduce((acc, domain) => {
+    const count = _.get(acc, domain, 0) + 1;
+    return { ...acc, [domain]: count };
+  }, {});
+
+
+const ggg = [
+  'info@gmail.com',
+  'info@yandex.ru',
+  'info@hotmail.com',
+  'mk@host.com',
+  'support@hexlet.io',
+  'key@yandex.ru',
+  'sergey@gmail.com',
+  'vovan@gmail.com',
+  'vovan@hotmail.com',
+];
+
+console.log(getFreeDomainsCount(ggg));
